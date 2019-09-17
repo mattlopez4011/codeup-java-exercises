b0198e3db3ed5c5074efc898a621ea8f7cfbca1f
Matt Lopez <matthew.lopez4011@gmail.com>
2019-09-17 15:45:06 -0500

    Completed Fizzbuzz exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -37,5 +37,44 @@ public class ControlFlowExercises {
 //        }
 
 
-    }
+//==========Fizzbuzz exercise
+
+//        Write a program that prints the numbers from 1 to 100.
+//        for (int i = 1; i <= 100; i++){
+//            System.out.println(i);
+//        }
+
+//        For multiples of three: print “Fizz” instead of the number.
+//        for (int i = 1; i <= 100; i++){
+//
+//            if (i % 3 == 0){
+//                System.out.println("Fizz");
+//            }else{
+//                System.out.println(i);
+//            }
+//        }
+
+//        For the multiples of five: print “Buzz”.
+//        for (int i = 1; i <= 100; i++){
+//
+//            if (i % 5 == 0){
+//                System.out.println("Buzz");
+//            }else{
+//                System.out.println(i);
+//            }
+//        }
+
+//        For numbers which are multiples of both three and five: print “FizzBuzz”.
+        for (int i = 1; i <= 100; i++){
+
+            if (i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzBuzz");
+            }else{
+                System.out.println(i);
+            }
+        }
+
+
+
+    } // End of Main class
 }
