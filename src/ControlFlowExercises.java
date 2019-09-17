6c106a63604a7a477523bf96c4b79930f9801d55
Matt Lopez <matthew.lopez4011@gmail.com>
2019-09-17 15:18:31 -0500

    completed Do While loop exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,13 +3,23 @@ import java.util.Arrays;
 public class ControlFlowExercises {
     public static void main(String[] args) {
 //        Loop Basics
-//        While
+//        Do While
 
-        int i = 5;
-        while(i <= 15){
-            System.out.format("%d ", i );
-            i++;
-
+//        int i = 5;
+//        while(i <= 15){
+//            System.out.format("%d ", i );
+//            i++;
+//
+//        }
+        int i = 2;
+        do {
+            System.out.println(i);
+            i *= Math.pow(i, 1);
         }
+        while (i <= 1000000);
+
+
+
+
     }
 }
