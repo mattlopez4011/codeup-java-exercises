29d3159a0af849cecc58f4772c3540e3e13fb4cf
Matt Lopez <matthew.lopez4011@gmail.com>
2019-09-17 14:55:28 -0500

    java while loop exercise

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+//        Loop Basics
+//        While
+
+        int i = 5;
+        while(i <= 15){
+            System.out.format("%d ", i );
+            i++;
+
+        }
+    }
+}
