3ac24ee01da426c8b838f414da4d93c133fe4404
Matt Lopez <matthew.lopez4011@gmail.com>
2019-09-18 12:32:55 -0500

    Started Control Statements and Loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -67,27 +67,53 @@ public class ControlFlowExercises {
 //        }
 
 //        For numbers which are multiples of both three and five: print “FizzBuzz”.
-//        for (int i = 1; i <= 100; i++){
-//
-//            if (i % 3 == 0 && i % 5 == 0){
-//                System.out.println("FizzBuzz");
-//            }else if (i % 3 == 0){
-//                System.out.println("Fizz");
-//            }else if (i % 5 == 0){
-//                System.out.println("Buzz");
-//            }
-//            else{
-//                System.out.println(i);
-//            }
-//        }
+        for (int i = 1; i <= 100; i++){
+
+            if (i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzBuzz");
+            }else if (i % 3 == 0){
+                System.out.println("Fizz");
+            }else if (i % 5 == 0){
+                System.out.println("Buzz");
+            }
+            else{
+                System.out.println(i);
+            }
+        }
 
 //========Display a table of powers.
 //        Prompt the user to enter an integer.
-        System.out.println("Enter your number...");
-        int userNum =  Integer.parseInt(input.nextLine());
-        System.out.println(userNum);
 
+//        do {
+//            System.out.println("What number would you like to go up to?");
+//            int userNum =  Integer.parseInt(input.nextLine());
+////            System.out.println(userNum); // Shows the users input
+//            System.out.println(userNum);
+//            if (userNum.isEmpty()){
+//                System.out.println("OOPS! Enter a number...");
+//            }else{
+//                System.out.printf("number");
+//                System.out.printf("- - - - - -");
+//                for (int i = 1; i <= userNum; i++ ){
+//                    System.out.println(i);
+//
+//                }
+//
+//            }
 
+//                Ask user if they would like to continue
+//                System.out.println("Would you like to continue?");
+//                String userContinue = input.nextLine();
+//                Sting userYes = "yes";
+
+//        }while(true);
+//
+//boolean userInput = false;
+
+//        ==================================================
+        // convert word to pig latin word (assume word doesn't begin with a vowel or end in ay)
+//        String cat = "cat"; // "atcay"
+//        String pigLatinCar = ;
 
 
     } // End of Main class
