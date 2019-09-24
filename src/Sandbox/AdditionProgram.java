1091a7a3fc4fe6c07ae7bdaa596e87cf3130395a
Matt Lopez <matthew.lopez4011@gmail.com>
2019-09-24 18:48:15 -0500

    Finished playing with constructors

--- src/Sandbox/AdditionProgram.java
+++ src/Sandbox/AdditionProgram.java
@@ -1,4 +1,10 @@
 package Sandbox;
 
 public class AdditionProgram {
+    public static void main(String[] args){
+//        Creating new object ⬇️
+        Addition addObject = new Addition(5,5);// Takes in two number parameter
+//        Below I am calling the new addObject and the getNumber Method to get the object value we assigned it.
+        System.out.println("The addition equals to: " + addObject.getNumber() );
+    }
 }
