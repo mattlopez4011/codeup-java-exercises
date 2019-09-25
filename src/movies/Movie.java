752397d3a4c6b6f2180dabe2ee1db7607fa93180
Matt Lopez <matthew.lopez4011@gmail.com>
2019-09-25 16:58:14 -0500

    Add MoviesArray file

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -1,19 +1,29 @@
 package movies;
 
 public class Movie {
-    private String name;
+    private String titleName;
     private String category;
 
-//    Constructor
-    public  Movie(String name, String category){
-        this.name = name;
+//===========Constructor
+    public  Movie(String title, String category){
+        this.titleName = title;
         this.category = category;
 
     }
 
-    public static void main(String[] args) {
-        System.out.println(createMovie("Harry Potter ", "Thriller"));
+    //========= Methods
+    // returns the person's name
+    public String getMovie(){
+
+        return this.titleName + this.category;
     }
+    // changes the name property to the passed value
+    public void setMovie(String title, String category){
+
+        this.titleName = title;
+        this.category = category;
+    }
+
 
 
 
