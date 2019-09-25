e2302559d212a0ed9661bc1b2932317d174a7596
Matt Lopez <matthew.lopez4011@gmail.com>
2019-09-25 16:45:13 -0500

    Start Movie list exercise

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -1,5 +1,20 @@
 package movies;
 
 public class Movie {
-    private static String name;
+    private String name;
+    private String category;
+
+//    Constructor
+    public  Movie(String name, String category){
+        this.name = name;
+        this.category = category;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(createMovie("Harry Potter ", "Thriller"));
+    }
+
+
+
 }
